ddd6512930108e717de09a7fb290f0497bb11f0c
Rinwu1114 <Rinwu1114@gmail.com>
2025-11-16 23:18:36 -0600

    added Features section, will work on reactiveness at the end, next section is stats section.

--- rin-advanced-internship/app/Landing/page.tsx
+++ rin-advanced-internship/app/Landing/page.tsx
@@ -1,9 +1,11 @@
 import LandingComponent from "./components/Landing";
+import Features from "./components/Features";
 
 export default function Landing() {
   return (
     <>
       <LandingComponent />
+      <Features />
     </>
   );
 }
