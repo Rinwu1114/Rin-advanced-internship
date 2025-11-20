5edd6094e5adaf6d0046ccccae05c49fd9cd72bd
Rinwu1114 <Rinwu1114@gmail.com>
2025-11-20 17:48:59 -0600

    Finished reviews section. Used static stars since it doesn't seem there is a dynamic api to hit for reviews and is not changing. changed nessessary css into tailwind.

---
+++ rin-advanced-internship/app/Landing/components/Reviews.tsx
@@ -0,0 +1,88 @@
+import { BsStarFill } from "react-icons/bs";
+
+export default function Reviews() {
+  return (
+    <section id="reviews">
+      <div className="row">
+        <div className="container">
+          <div className="text-3xl text-[#032b41] text-center mb-8 font-bold">What our members say</div>
+          <div className="max-w-[600px] mx-auto">
+            <div className="review bg-[#fff3d7] p-4 mb-8 rounded-sm font-light">
+              <div className="text-[#032b41] flex gap-2 mb-2">
+                <div className="review__name">Hanna M.</div>
+                <div className="flex">
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                </div>
+              </div>
+              <div className="text-[#394547] tracking-[0.3px] leading-[1.4]">
+                This app has been a <b>game-changer</b> for me! It's saved me so
+                much time and effort in reading and comprehending books. Highly
+                recommend it to all book lovers.
+              </div>
+            </div>
+            <div className="review bg-[#fff3d7] p-4 mb-8 rounded-sm font-light">
+              <div className="text-[#032b41] flex gap-2 mb-2">
+                <div className="review__name">David B.</div>
+                <div className="flex">
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                </div>
+              </div>
+              <div className="text-[#394547] tracking-[0.3px] leading-[1.4]">
+                I love this app! It provides
+                <b>concise and accurate summaries</b> of books in a way that is
+                easy to understand. It's also very user-friendly and intuitive.
+              </div>
+            </div>
+            <div className="review bg-[#fff3d7] p-4 mb-8 rounded-sm font-light">
+              <div className="text-[#032b41] flex gap-2 mb-2">
+                <div className="review__name">Nathan S.</div>
+                <div className="flex">
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                </div>
+              </div>
+              <div className="text-[#394547] tracking-[0.3px] leading-[1.4]">
+                This app is a great way to get the main takeaways from a book
+                without having to read the entire thing.
+                <b>The summaries are well-written and informative.</b>
+                Definitely worth downloading.
+              </div>
+            </div>
+            <div className="review bg-[#fff3d7] p-4 mb-8 rounded-sm font-light">
+              <div className="text-[#032b41] flex gap-2 mb-2">
+                <div className="review__name">Ryan R.</div>
+                <div className="flex">
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                  <BsStarFill className="w-4 h-4 fill-[#0564f1]"/>
+                </div>
+              </div>
+              <div className="text-[#394547] tracking-[0.3px] leading-[1.4]">
+                If you're a busy person who
+                <b>loves reading but doesn't have the time</b> to read every
+                book in full, this app is for you! The summaries are thorough
+                and provide a great overview of the book's content.
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <button className="btn home__cta--btn">Login</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
