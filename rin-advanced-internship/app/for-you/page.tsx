938b1f976a805be64e9bb0f0c1bc0d669bee1907
Rinwu1114 <Rinwu1114@gmail.com>
2025-12-14 19:07:43 -0600

    added recommened section. bugfixed reactivity issue with grid format on page.tsx. moved all row and continer classes into page.tsx. added css class hide-scrollbar and .hide-scrollbar::webit-scrollbar in globals to hide scrollbar for future use.

--- rin-advanced-internship/app/for-you/page.tsx
+++ rin-advanced-internship/app/for-you/page.tsx
@@ -1,15 +1,23 @@
-import Sidebar from "../components/Sidebar"
-import Search from "../components/Search"
-import Selected from "./components/Selected"
+import Sidebar from "../components/Sidebar";
+import Search from "../components/Search";
+import Selected from "./components/Selected";
+import Recommended from "./components/Recommended";
 
 export default function forYou() {
-    return ( 
-        <div className="grid grid-cols-[200px_1fr]">
-       <Sidebar />
-       <div>
-       <Search />
-       <Selected />
-       </div>
+  return (
+    <div className="grid grid-cols-[200px_minmax(0,1fr)]">
+      <Sidebar />
+      <div>
+        <Search />
+        <div className="row">
+          <div className="container">
+            <div className="for-you__wrapper">
+              <Selected />
+              <Recommended />
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
