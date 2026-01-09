2aa87f9c51432493dce27e3142e57bd3f33e987e
Rinwu1114 <Rinwu1114@gmail.com>
2026-01-09 17:47:18 -0600

    added premium for premium books to show if user is not logged in or is basic user. rerouted book buttons to link to choose plan if user is logged in but not premium and to open popup if use is not logged in.

--- rin-advanced-internship/app/book/[id]/components/BookButtons.tsx
+++ rin-advanced-internship/app/book/[id]/components/BookButtons.tsx
@@ -2,21 +2,51 @@
 import { useRouter } from "next/navigation";
 import { LuBookOpenText } from "react-icons/lu";
 import { IoMicOutline } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/app/redux/store";
+import { openPopUp } from "@/app/redux/slices/loginSlice";
 
 export default function BookButtons({ bookId }: { bookId: string }) {
-    const router = useRouter();
-    return (
-        <div className="flex gap-4 mb-6">
-            <button className="btns flex items-center justify-center w-36 h-12 bg-[#032b41] text-[#fff] rounded-sm cursor-pointer gap-2 transition-opacity duration-150 hover:opacity-80" 
-            onClick={() => router.push(`/player/${bookId}`)}>
-                <div className="read__icon flex"><LuBookOpenText className="w-6 h-6"/></div>
-                <div className="read__text">Read</div>
-            </button>
-            <button className="audio__btn flex items-center justify-center w-36 h-12 bg-[#032b41] text-[#fff] rounded-sm cursor-pointer gap-2 transition-opacity duration-150 hover:opacity-80" 
-            onClick={() => router.push(`/player/${bookId}`)}>
-                <div className="audio__icon flex"><IoMicOutline className="w-6 h-6"/></div>
-                <div className="audio__text">Listen</div>
-            </button>
+  const dispatch = useDispatch();
+
+  const user = useSelector((state: RootState) => state.AuthState.user);
+  const router = useRouter();
+
+  const handleBookButtonClick = () => {
+    if (user?.plan === undefined) {
+      dispatch(openPopUp());
+      return;
+    }
+    if (user?.plan === "Basic") {
+      router.push(`/choose-plan`);
+      return;
+    } else {
+      {
+        router.push(`/player/${bookId}`);
+      }
+    }
+  };
+
+  return (
+    <div className="flex gap-4 mb-6">
+      <button
+        className="btns flex items-center justify-center w-36 h-12 bg-[#032b41] text-[#fff] rounded-sm cursor-pointer gap-2 transition-opacity duration-150 hover:opacity-80"
+        onClick={handleBookButtonClick}
+      >
+        <div className="read__icon flex">
+          <LuBookOpenText className="w-6 h-6" />
         </div>
-    );
+        <div className="read__text">Read</div>
+      </button>
+      <button
+        className="audio__btn flex items-center justify-center w-36 h-12 bg-[#032b41] text-[#fff] rounded-sm cursor-pointer gap-2 transition-opacity duration-150 hover:opacity-80"
+        onClick={handleBookButtonClick}
+      >
+        <div className="audio__icon flex">
+          <IoMicOutline className="w-6 h-6" />
+        </div>
+        <div className="audio__text">Listen</div>
+      </button>
+    </div>
+  );
 }
